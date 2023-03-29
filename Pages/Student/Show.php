0d53bddcb0e41da09cf32e321275e5592cfcb713
Phuong03 <phuong.zorum24@student.passerellesnumeriques.org>
2023-03-29 20:38:36 +0700

    Hienthi Student

--- Pages/Student/Show.php
+++ Pages/Student/Show.php
@@ -7,13 +7,10 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../../Styles/Student/Show.css">
   <link rel="stylesheet" href="../../Styles/inc_styles/style_footer.css">
-
- 
   <title>trang cá nhân của học sinh</title>
+  
 </head>
 <body>
-  <header>
-  </header>
 
 <br>
 <br>
@@ -21,9 +18,9 @@
     <div class="container-fluid" style="padding-top: 30px;">
       <div class="row">
         <div class="col-sm-1"></div>
-        <div class="col-sm-5">
+        <div class="col-sm-5 ">
           <h4 id="sb" class="sb" style="font: size 45px;" >Thông tin cá nhân của bạn</h4>
-
+          <div class="info d-flex">
           <div>
             <i class="bi bi-person">
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person"
@@ -31,31 +28,15 @@
                 <path
                   d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
               </svg>
-            </i>
+            </i> 
           </div>
-          <br>
           <div>
-            <i class="bi bi-geo-alt">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-geo-alt"
-                viewBox="0 0 16 16" name="address">
-                <path
-                  d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-              </svg>
-            </i>
+            <p><?php echo $row['Full_name'] ?></p>
+          </div>
           </div>
           <br>
-          <div>
-            <i class="bi bi-phone">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-phone"
-                viewBox="0 0 16 16" name="phone">
-                <path
-                  d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
-                <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-              </svg>
-            </i>
-          </div>
           <br>
+          <div class="info d-flex">
           <div>
             <i class="bi bi-envelope-at">
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
@@ -66,6 +47,8 @@
                   d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z" />
               </svg>
             </i>
+          </div>
+          <div><p><?php echo $row['Email'] ?></p></div>
           </div>
         </div>
       
@@ -113,7 +96,7 @@
           </div>
         </div>
       </div>
-    </div>
+   
     <div class="col-sm-1"></div>
   </div>
 </div>
@@ -122,18 +105,19 @@
 
 
   <footer>
-  <div class="container">
-    <div class="footer">	
-        <div class="content-footer">
-            <div class="footer-left">
+  <div class="container-fliud" id="footer" >
+    <div class="row" style="background-color:#27D3BF">	
+      <div class="col-sm-1"></div>
+        <div class="col-sm-4">
+           
                 <h3>Dịch vụ gia sư KingDom</h3>
                 <p><img src="../images/location.png" alt="" width="30"> Địa chỉ: 101b Lê Hữu Trác, Phước Mỹ, Sơn Trà, Đà Nẵng</p>
                 <p><img src="../images/gmail.png" alt="" width="30"> Email: TienPM24@gmail.com</p>
                 <p><img src="../images/call.png" alt="" width="30"> Hỗ trợ học sinh: 0988888888 (Lực)</p>
                 <p><img src="../images/call.png" alt="" width="30"> Hỗ trợ gia sư: 0988888888 (Tiến PM)</p>
-            </div>
-            <div class="footer-right">
-                <div class="right-content">
+                </div>
+           
+                <div class="col-sm-4">
                     <br>
                     <br>
                     <h3>Thời gian làm việc</h3>
@@ -144,27 +128,29 @@
                     <p>Thứ 6: 8h00-21h00</p>
                 </div>
                 <div class="left-item">
-                    <img src="../images/payment.png" alt="" >
-                    <img src="../images/Bank.png" alt="" >
+                    <img src="../../images/payment.png" alt="" >
+                    <img src="../../images/Bank.png" alt="" >
                 </div>        
             </div>
         </div>
-    </div>			
-    <div class="contact_top">
+    	
+    <div class="col-sm-1"></div>
+    <div class="row"style="background-color:#27D3BF">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-9">
+    <div class="contact_top" >
         <a>Flow us:</a> 
         <a href="https://www.facebook.com"><img src="../images/Facebook.png" alt="" width="30"> Facebook</a>
         <a href="https://www.tiktok.com"><img src="../images/Tiktok.png" alt="" width="30"> Tiktok</a>
         <a href="skype:yourusername?chat"><img src="../images/Skype.png" alt="" width="30"> Skype</a>
         <a href="https://www.youtube.com"><img src="../images/Youtube.png" alt="" width="30"> Youtube</a>
   </div>
+  </div>	
+  </div>
   </br>
     </div>
   </footer>
 </body>
-
-
-
-
 </html>
 
 
--- Pages/Student/sinhvien.php
+++ Pages/Student/sinhvien.php
@@ -1,6 +1,5 @@
 <?php
   include "./Database/User.class.php";
-
   $student = new Student();
   $row=$student->Display();
 ?>
