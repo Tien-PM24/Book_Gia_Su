7f55e540d6c7a60ae1cfd7fa204432a44a3c48a5
Phuong03 <phuong.zorum24@student.passerellesnumeriques.org>
2023-04-01 23:50:11 +0700

    Hien thi các khoa hoc dang theo học cua hoc sinh

--- Pages/Student/Show.php
+++ Pages/Student/Show.php
@@ -52,8 +52,7 @@
     
 <br>
 <br>
-    <div class="card-body">
-      
+    
     <?php
             $servername = "localhost";
             $username = "root";
@@ -67,28 +66,71 @@
             // Thực hiện truy vấn đến cơ sở dữ liệu
             $sql = "SELECT *FROM student";
             $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) ) {
+            if (mysqli_num_rows($result) >0 ) {
                 // Duyệt qua các bản ghi của kết quả và show thông tin giáo viên
                 while($row = mysqli_fetch_assoc($result)) {
-                    echo "<h2>" . $row["Full_name"] . "</h2>";
-                    echo "<p>" . $row["Email"] . "</p>";
-                    echo '<p>'.$row["Job_title"].'</p>';
+                    echo '<div class="card-body">';
+                    echo "<p>" . $row["Full_name"] . "</p>";
+                    echo '<p>'.$row["Email"].'</p>';
+                    echo '<p>'.$row["Address"].'</p>';
+                    // echo '<p>'.$row["Job_title"].'</p>';
                 }
-                return $result;
+               
             } else {
                 echo "Không có kết quả";
             };
         ?>
         <div class="card-button-wrap">
             <button class="card-btn card-btn--secondary">
-            <a href="edit.php?id=<?php echo $i?>">Sửa</a>
+            Sửa
             </button>
             <button class="card-btn card-btn--primary">
-            <a href="delete.php?id=<?php echo $i?>">Xóa</a>
+            Xóa
             </button>
         </div>
     </div>
 </div>
+
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "tutors";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Kết nối đến cơ sở dữ liệu thất bại: " . mysqli_connect_error());
+    }
+    
+    // Thực hiện truy vấn đến cơ sở dữ liệu
+    $sql = "SELECT course.*, student.*, student_course.*
+            FROM course
+            INNER JOIN student_course ON course.ID_course = student_course.ID_course
+            INNER JOIN student ON student.ID_student = student_course.ID_student";
+    $result = mysqli_query($conn, $sql);
+    
+    if ($result && mysqli_num_rows($result) > 0 ) {
+        // Duyệt qua các bản ghi của kết quả và show thông tin giáo viên
+        while($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="grid-container">';
+            echo '<div class="card">';
+            echo '<img src="'.$row['Image'] . '"/>';
+            echo '<div class="card-content">';
+            echo '<p>'.$row["Body"].'</p>';
+            echo '<p>'.$row["Name"].'</p>';
+            echo '<p>'.$row["Price"].'</p>';
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+        }
+       
+    } else {
+        echo "Không có kết quả";
+    }
+    
+    mysqli_close($conn);
+?>
+
+        
     <footer>
 
     </footer>
