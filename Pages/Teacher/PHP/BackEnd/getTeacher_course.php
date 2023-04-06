5ae1dc35e012ff0b2ebe9c939aae5936dabc46cb
Ha2k3 <vothuyha2003@gmail.com>
2023-04-06 09:10:18 +0700

    updatePageTutor

--- Pages/Teacher/PHP/BackEnd/getTeacher_course.php
+++ Pages/Teacher/PHP/BackEnd/getTeacher_course.php
@@ -13,11 +13,12 @@ if (mysqli_num_rows($result) > 0) {
     // Duyệt qua các bản ghi của kết quả và show thông tin giáo viên
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<div class='course'>";
-        echo '<p class="pshow"><strong>Hình ảnh</strong><br><img src="' . $row['Image'] . '"width =100 height=100></p>';
+        echo '<p class="pshow"><strong>Hình ảnh</strong><br><img src="'.$row['Image'].'"width =100 height=100></p>';
         echo "<p class='pshow'><strong>Tên khóa học:</strong> " . $row["Name"] . "</p>";
         echo "<p class='pshow'><strong>Giá:</strong> " . $row["Price"] . "</p>";
         echo "<p class='pshow'><strong>Mô tả:</strong> " . $row["Body"] . "</p>";
         echo "</div>";
+        
     }
 } else {
     echo "Không có kết quả";
