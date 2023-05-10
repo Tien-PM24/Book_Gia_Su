82cf1d77d2ad2975d37091eaada88777f319a8fb
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-05-10 12:05:05 +0700

    I was change all our project to finish project

--- Pages/Admin/Php/FrontEnd/Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "./Admin.php";
-include "../BackEnd/Show_chart.php";
-?>
-
-<script>
-    var Home = document.querySelector("._menu_home__");
-    Home.style.background = "#FFA500";
-</script>
