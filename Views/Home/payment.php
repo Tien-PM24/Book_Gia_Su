59d06292673989666b3d89a6ee2eb895e4b668e7
Phan Thanh Luc <phanthanhluc24@gmail.com>
2023-05-11 20:11:37 +0700

    Finish our project

--- Views/Home/payment.php
+++ Views/Home/payment.php
@@ -1,79 +1,39 @@
 <?php
 include "./header.php";
-include "../../Database/connectDB.php";
+include "../../Database/connectBS.php";
+if(isset($_GET['id'])) {
+    $id=$_GET['id'];
+    $sql = "SELECT * FROM course WHERE id_course = $id";
+    $result = mysqli_query ($conn,$sql);
+    $row = mysqli_fetch_assoc($result);
+}
 
-class Payment extends ConnectDB {
-    public function getPayment($id) {
-        $conn = $this->connection;
-        $sql = "SELECT * FROM course WHERE id_course = $id";
-        $result = mysqli_query ($conn,$sql);
-        $row = mysqli_fetch_assoc($result);
-        return $row;
+if(isset($_POST['payment'])){
+    $emailUser=$_SESSION['user'];
+    $sql1 = "SELECT id_student,email FROM student WHERE email = '$emailUser'";
+    $result1 = mysqli_query($conn, $sql1);
+    $row1 = mysqli_fetch_assoc($result1);
+    $id_student=$row1['id_student'];
+    $id_course=$_POST['id_course'];
+    $price=$_POST['price'];
+
+    $sql2="SELECT teacher.id_teacher as id_teacher from teacher,course,teacher_course 
+    where teacher.id_teacher=teacher_course.id_teacher and course.id_course=teacher_course.id_course
+    and teacher_course.id_course=$id_course";
+    $result2 = mysqli_query($conn, $sql2);
+    $row2 = mysqli_fetch_assoc($result2);
+    $id_teacher=$row2['id_teacher'];
+
+    if($emailUser===$row1['email']){
+        $sql3="INSERT into payment (id_course, id_student, price, payment_day) 
+        Values (' $id_course','$id_student','$price',Now())";
+        $result3 = mysqli_query($conn, $sql3);
+
+        $sql4="INSERT into student_teacher(id_teacher,id_student)
+        values ('$id_teacher','$id_student')
+        ";
+         $result4 = mysqli_query($conn, $sql4);
     }
-
-    public function getTeacher($id) {
-        $conn = $this->connection;
-        $sql4 = "SELECT DISTINCT teacher.id_teacher as Teacher
-        FROM teacher,teacher_course,course
-        WHERE teacher.id_teacher=teacher_course.id_teacher
-        AND course.id_course=teacher_course.id_course
-        AND course.id_course=$id";
-        $result4 = mysqli_query ($conn,$sql4);
-        $row4 = mysqli_fetch_assoc($result4);
-        $id_teacher = $row4['id_teacher'];
-        return $id_teacher;
-    }    
-
-    public function bookCourse($id_course, $email_user, $price) {
-        $conn = $this->connection;
-        $sql1 = "SELECT id_student FROM student WHERE email = '$email_user'";
-        $result1 = mysqli_query($conn, $sql1);
-        $row1 = mysqli_fetch_assoc($result1);
-        $id_student = $row1['id_student'];
-    
-        $sql2 = "SELECT id_student FROM payment WHERE id_student = '$id_student' AND id_course = '$id_course'";
-        $result2 = mysqli_query($conn, $sql2);
-        $row2 = mysqli_fetch_assoc($result2);
-        if ($row2) {
-            echo "<script>
-                    alert('You have already booked this course!');
-                    window.location.href = './course_page.php';
-                  </script>";
-        } else {
-            $sql3 = "INSERT INTO payment (id_course, id_student, price, payment_day) 
-                    VALUES ('$id_course', '$id_student', '$price', NOW())";
-            $result3 = mysqli_query($conn, $sql3);
-    
-            if ($result3 === TRUE) {
-                echo "<script>
-                        alert('You booked this course successfully!');
-                        window.location.href = './course_page.php';
-                      </script>";
-            } else {
-                echo "Error: " . $sql3 . "<br>" . $conn->error;
-            }
-    
-            $id_teacher = $this->getTeacher();
-            $sql4 = "INSERT INTO student_teacher VALUES ('$id_student','$id_teacher')";
-            $result4 = mysqli_query($conn, $sql4);
-        }
-    }    
-    
-}
-
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-if (!$id) {
-    exit;
-}
-$payment = new Payment();
-$row = $payment->getPayment($id);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $id_course = $_POST['id_course'];
-    $email_user = $_SESSION['user'];
-    $price = $_POST['price'];
-
-    $payment->bookCourse($id_course, $email_user, $price);
 }
 ?>
 
@@ -100,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <form method="POST">
                     <input type="hidden" name="id_course" value="<?php echo $id; ?>">
                     <input type="hidden" name="price" value="<?php echo $row['price']; ?>">
-                    <button type="submit" class="btn btn-primary">Đồng ý</button>
+                    <button name="payment" type="submit" class="btn btn-primary">Đồng ý</button>
                 </form>
             </div>
         </div>
