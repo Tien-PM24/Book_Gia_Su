9428b9d8a5863c0b656c33bd5ae309c0411276e7
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-05-15 08:43:16 +0700

    Add new item

--- payment.php
+++ payment.php
@@ -1,74 +1,78 @@
     <?php
     session_start();
-    $emailUser = $_SESSION['user'];
+    $emailU_ser = $_SESSION['user'];
 
     include "./src/core/connectDB.php";
 
-        class Payment extends ConnectDB {
-            public function getPayment($id) {
-                $conn = $this->connection;
-                $sql = "SELECT * FROM course WHERE id_course = $id";
-                $result = mysqli_query ($conn,$sql);
-                $row = mysqli_fetch_assoc($result);
-                return $row;
-            }
+    class Payment extends ConnectDB {
+
+        public function getPayment($id) {
+            $conn = $this->connection;
+            $sql = "SELECT * FROM course WHERE id_course = $id";
+            $result = mysqli_query ($conn,$sql);
+            $row = mysqli_fetch_assoc($result);
+            return $row;
+        }
+
+        public function getTeacher($id) {
+            $conn = $this->connection;
+            $sql4 = "SELECT DISTINCT teacher.id_teacher
+                    FROM teacher,teacher_course,course
+                    WHERE teacher.id_teacher=teacher_course.id_teacher
+                    AND course.id_course=teacher_course.id_course
+                    AND course.id_course=$id";
+            $result4 = mysqli_query ($conn,$sql4);
+            $row4 = mysqli_fetch_assoc($result4);
+            $id_teacher = $row4['id_teacher'];
+            return $id_teacher;
+        }
     
-            public function getTeacher($id) {
-                $conn = $this->connection;
-                $sql4 = "SELECT DISTINCT teacher.id_teacher
-                FROM teacher,teacher_course,course
-                WHERE teacher.id_teacher=teacher_course.id_teacher
-                AND course.id_course=teacher_course.id_course
-                AND course.id_course=$id";
-                $result4 = mysqli_query ($conn,$sql4);
-                $row4 = mysqli_fetch_assoc($result4);
-                $id_teacher = $row4['id_teacher'];
-                return $id_teacher;
-            }    
+        public function bookCourse($id_course, $email_user, $price) {
+            $conn = $this->connection;
+            $sql1 = "SELECT id_student FROM student WHERE email = '$email_user'";
+            $result1 = mysqli_query($conn, $sql1);
+            $row1 = mysqli_fetch_assoc($result1);
+            $id_student = $row1['id_student'];
     
-            public function bookCourse($id_course, $email_user, $price) {
-                $conn = $this->connection;
-                $sql1 = "SELECT id_student FROM student WHERE email = '$email_user'";
-                $result1 = mysqli_query($conn, $sql1);
-                $row1 = mysqli_fetch_assoc($result1);
-                $id_student = $row1['id_student'];
-            
-                $sql2 = "SELECT id_student FROM payment WHERE id_student = '$id_student' AND id_course = '$id_course'";
-                $result2 = mysqli_query($conn, $sql2);
-                $row2 = mysqli_fetch_assoc($result2);
-                if ($row2) {
+            $sql2 = "SELECT id_student FROM payment WHERE id_student = '$id_student' AND id_course = '$id_course'";
+            $result2 = mysqli_query($conn, $sql2);
+            $row2 = mysqli_fetch_assoc($result2);
+            if ($row2) {
+                echo "<script>
+                        alert('You have already booked this course!');
+                        window.location.href = './course_page.php';
+                    </script>";
+            } else {
+                $sql3 = "INSERT INTO payment (id_course, id_student, price, payment_date) 
+                        VALUES ('$id_course', '$id_student', '$price', NOW())";
+                $result3 = mysqli_query($conn, $sql3);
+    
+                if ($result3 === TRUE) {
                     echo "<script>
-                            alert('You have already booked this course!');
+                            alert('You booked this course successfully!');
                             window.location.href = './course_page.php';
                         </script>";
                 } else {
-                    $sql3 = "INSERT INTO payment (id_course, id_student, price, payment_date) 
-                            VALUES ('$id_course', '$id_student', '$price', NOW())";
-                    $result3 = mysqli_query($conn, $sql3);
-
-
-            
-                    if ($result3 === TRUE) {
-                        echo "<script>
-                                alert('You booked this course successfully!');
-                                window.location.href = './course_page.php';
-                            </script>";
-                    } else {
-                        echo "Error: " . $sql3 . "<br>" . $conn->error;
-                    }
-            
-                    $id_teacher = $this->getTeacher();
-                    $sql4 = "INSERT INTO student_teacher VALUES ('$id_student','$id_teacher')";
-                    $result4 = mysqli_query($conn, $sql4);
+                    echo "Error: " . $sql3 . "<br>" . $conn->error;
                 }
-            }    
-            
-        }
+
+                //$id_teacher = $this->getTeacher($id_course);
+                //$sql4 = "INSERT INTO student_teacher VALUES ('$id_student','$id_teacher')";
+                //$result4 = mysqli_query($conn, $sql4);
+
+                $id_teacher = $this->getTeacher($id_course);
+                $sql4 = "INSERT INTO student_teacher VALUES ('$id_student','$id_teacher')";
+                $result4 = mysqli_query($conn, $sql4);
+
+            }
+        }    
+    }
     
-        $id = isset($_GET['id']) ? $_GET['id'] : null;
-        if (!$id) {
-            exit;
-        }
+    $id = isset($_GET['id']) ? $_GET['id'] : null;
+    if (!$id) {
+        exit;
+    }
+
         $payment = new Payment();
         $row = $payment->getPayment($id);
     
